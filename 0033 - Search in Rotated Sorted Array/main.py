63a5b9d6b4cbbdce4c7f2c78a2873fc5c80b5578
yuri <yuri.bertoldi003@gmail.com>
2025-03-19 11:35:09 -0300

    explicacao

--- 0033 - Search in Rotated Sorted Array/main.py
+++ 0033 - Search in Rotated Sorted Array/main.py
@@ -1,8 +1,35 @@
+"""
+# uma lista ordenada é do tipo [1,2,3,4,5]
+# rotacionada uma vez ela fica [5,1,2,3,4]
+# duas vezes: [4,5,1,2,3]
+# com cinco vezes ela volta ao estado original
+
+toda lista ordenada rotacionada se encaixa em dois casos:
+1 - ela é idêntica a original
+2 - ela possui duas partes ordenadas e um trecho de disrupção que separa as duas. 
+    A parte ordenada da esquerda tem sempre números maiores que da direita.
+
+Por exemplo:
+Lista: [5, 6, 7, 1, 2, 3, 4]
+trecho da esquerda (maior): [5, 6, 7]
+trecho da direita (menor): [1, 2, 3, 4]
+trecho de disrupção: [7, 1]
+
+O trecho de disrupção é o único onde um número é maior que seu sucessor. 
+Em todas as outras partes da lista, um número é sempre sucedido por outro maior.
+
+"""
+
+
+
 def search(nums, target):
     l, r = 0, len(nums) - 1
+    
     while l <= r:
+        
         m = (l + r) // 2
 
+        # binary search comum: caso da lista estar ordenada de l à r
         if nums[l] < nums[r]:
 
             if nums[m] == target:
@@ -12,13 +39,14 @@ def search(nums, target):
             else:
                 r = m - 1                 
         
+        # caso a lista não esteja ordenada de l à r
         else:
-            if target > nums[l]:
-                if nums[l] > nums[m]: 
+            if target > nums[l]: # caso o alvo esteja na parte esquerda (maior) da lista
+                if nums[l] > nums[m]: # caso haja disrupção de entre l e m da lista
                     r = m - 1
-                elif l == m:
+                elif l == m: # caso a lista tenha apenas 2 elementos com disrupção
                     r -= 1
-                else:
+                else: #caso não haja disrupção na lista
                     if target < nums[m]:
                         r = m - 1
                     elif target > nums[m]:
@@ -26,12 +54,12 @@ def search(nums, target):
                     else:
                         return m
                     
-            elif target < nums[l]: 
-                if nums[l] < nums[m]: 
+            elif target < nums[l]: # caso o alvo esteja na parte direita (menor) da lista
+                if nums[l] < nums[m]: # caso não haja disrupção de l a m
                     l = m + 1
-                elif l == m:
+                elif l == m: # caso a lista tenha 2 elementos
                     l += 1
-                else:
+                else: #caso haja disrupção entre l e m
                     if target > nums[m]:
                         l = m + 1
                     elif target < nums[m]:
