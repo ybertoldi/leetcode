4acf56315c0d486487189692d1d56d37b0a3e7f3
yuri <yuri.bertoldi003@gmail.com>
2025-03-19 11:39:15 -0300

    explicacao

--- 0033 - Search in Rotated Sorted Array/main.py
+++ 0033 - Search in Rotated Sorted Array/main.py
@@ -11,8 +11,8 @@ toda lista ordenada rotacionada se encaixa em dois casos:
 
 Por exemplo:
 Lista: [5, 6, 7, 1, 2, 3, 4]
-trecho da esquerda (maior): [5, 6, 7]
-trecho da direita (menor): [1, 2, 3, 4]
+parte ordenada da esquerda (maior): [5, 6, 7]
+parte ordenada da direita (menor): [1, 2, 3, 4]
 trecho de disrupção: [7, 1]
 
 O trecho de disrupção é o único onde um número é maior que seu sucessor. 
@@ -29,7 +29,7 @@ def search(nums, target):
         
         m = (l + r) // 2
 
-        # binary search comum: caso da lista estar ordenada de l à r
+        # binary search comum: caso da lista estar ordenada de l à r (ou seja, não possua disrupção)
         if nums[l] < nums[r]:
 
             if nums[m] == target:
@@ -39,14 +39,14 @@ def search(nums, target):
             else:
                 r = m - 1                 
         
-        # caso a lista não esteja ordenada de l à r
+        # caso a lista não esteja ordenada de l à r (ou seja, há uma disrupção entre l e r)
         else:
             if target > nums[l]: # caso o alvo esteja na parte esquerda (maior) da lista
                 if nums[l] > nums[m]: # caso haja disrupção de entre l e m da lista
                     r = m - 1
                 elif l == m: # caso a lista tenha apenas 2 elementos com disrupção
                     r -= 1
-                else: #caso não haja disrupção na lista
+                else:  #caso não haja disrupção entre l e m
                     if target < nums[m]:
                         r = m - 1
                     elif target > nums[m]:
@@ -55,7 +55,7 @@ def search(nums, target):
                         return m
                     
             elif target < nums[l]: # caso o alvo esteja na parte direita (menor) da lista
-                if nums[l] < nums[m]: # caso não haja disrupção de l a m
+                if nums[l] < nums[m]: # caso não haja disrupção entre l e m
                     l = m + 1
                 elif l == m: # caso a lista tenha 2 elementos
                     l += 1
